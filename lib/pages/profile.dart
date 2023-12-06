91972257f9021b8e1e2034abf2fb5e1d6574e095
Mohammed Abdelmegeed <m.maguid9@hotmail.com>
2023-12-06 21:38:21 +0200

    Reading & displaying profile data from SQFlite

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -20,21 +20,26 @@ class _ProfileState extends State<Profile> {
   Future Reading_Database() async {
     List<Map> response =
         await mydb.read(''' SELECT * FROM 'PASSENGERS' LIMIT 1 ''');
-    // userDocuments = [];
+    userDocuments = [];
     userDocuments.addAll(response);
     print("User Document:");
-    print("User Document:" + userDocuments[0]["NAME"]);
-    setState(() {});
+    print("User Name:" + userDocuments[0]["NAME"]);
+    // setState(() {});
   }
 
   @override
   void initState() {
-    _user = _auth.currentUser;
-    Reading_Database();
     super.initState();
+    _user = _auth.currentUser;
+    Reading_Database().then((_) {
+      setState(() {
+        isLoading = false;
+      });
+    });
     mydb.checkdata();
   }
 
+  bool isLoading = true;
   int _currentIndex = 3;
 
   @override
@@ -45,81 +50,85 @@ class _ProfileState extends State<Profile> {
         title: Text('My Profile'),
       ),
       body: Center(
-        child: Card(
-          shadowColor: Colors.black,
-          color: AppColors.primaryColorLight,
-          child: Container(
-            width: 300,
-            height: 450, // Adjust the width as needed
-            padding: EdgeInsets.all(16),
-            decoration: BoxDecoration(
-              border: Border.all(color: AppColors.secondaryColor, width: 2),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                CircleAvatar(
-                    backgroundColor: AppColors.secondaryColor,
-                    radius: 50,
-                    child: Icon(
-                      Icons.person, // Replace with the desired icon
-                      size: 85,
-                      color:
-                          Colors.white, // Replace with the desired icon color
-                    )),
-                SizedBox(height: 16),
-                Text(
-                  '${_user!.displayName}',
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w300,
-                      color: AppColors.textColor),
-                ),
-                SizedBox(height: 8),
-                Text(
-                  '${_user!.email}',
-                  style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w300,
-                      color: AppColors.textColor),
-                ),
-                SizedBox(height: 8),
-                // Text(
-                //   '${_user!.email}',
-                //   style: TextStyle(
-                //       fontSize: 16,
-                //       fontWeight: FontWeight.w300,
-                //       color: AppColors.textColor),
-                // ),
-                SizedBox(height: 50),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor:
-                          AppColors.backgroundCardColor, // Background color
-                      foregroundColor: Colors.white, // Text color
-                      padding: EdgeInsets.all(16), // Padding
-                      elevation: 5, // Elevation
-                      minimumSize: Size(200, 10)),
-                  onPressed: () async {
-                    await mydb.delete(''' DELETE FROM 'PASSENGERS' ''');
-                    await FirebaseAuth.instance.signOut();
-                    Navigator.pushReplacementNamed(context, '/login');
-                  },
-                  child: Text(
-                    'Sign Out',
-                    style: TextStyle(
-                      fontFamily: 'Roboto',
-                      color: Colors.black,
-                    ),
+        child: isLoading
+            ? CircularProgressIndicator()
+            : Card(
+                shadowColor: Colors.black,
+                color: AppColors.primaryColorLight,
+                child: Container(
+                  width: 300,
+                  height: 450, // Adjust the width as needed
+                  padding: EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    border:
+                        Border.all(color: AppColors.secondaryColor, width: 2),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      CircleAvatar(
+                          backgroundColor: AppColors.secondaryColor,
+                          radius: 50,
+                          child: Icon(
+                            Icons.person, // Replace with the desired icon
+                            size: 85,
+                            color: Colors
+                                .white, // Replace with the desired icon color
+                          )),
+                      SizedBox(height: 16),
+                      Text(
+                        // '${_user!.displayName}',
+                        '${userDocuments[0]["NAME"]}',
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w300,
+                            color: AppColors.textColor),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        '${userDocuments[0]["EMAIL"]}',
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w300,
+                            color: AppColors.textColor),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        '${userDocuments[0]["PHONENUMBER"]}',
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w300,
+                            color: AppColors.textColor),
+                      ),
+                      SizedBox(height: 50),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: AppColors
+                                .backgroundCardColor, // Background color
+                            foregroundColor: Colors.white, // Text color
+                            padding: EdgeInsets.all(16), // Padding
+                            elevation: 5, // Elevation
+                            minimumSize: Size(200, 10)),
+                        onPressed: () async {
+                          await mydb.delete(''' DELETE FROM 'PASSENGERS' ''');
+                          await FirebaseAuth.instance.signOut();
+                          Navigator.pushReplacementNamed(context, '/login');
+                        },
+                        child: Text(
+                          'Sign Out',
+                          style: TextStyle(
+                            fontFamily: 'Roboto',
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
-          ),
-        ),
+              ),
       ),
       bottomNavigationBar: NavBar(
         currentIndex: _currentIndex,
